b47453a4f55f86561e0d9bffe291ee8b5ee057b8
gdmec07131018 <763027293@qq.com>
2014-10-23 11:27:34 +0800

    Signed-off-by: gdmec07131018 <763027293@qq.com>

---
+++ src/cn/edu/gdmec/s07131018/intentfilter/MainActivity.java
@@ -0,0 +1,33 @@
+package cn.edu.gdmec.s07131018.intentfilter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    private EditText et;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et = (EditText) findViewById(R.id.et);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void Click(View view){
+    	Uri myUri = Uri.parse(et.getText().toString());
+    	Intent intent = new Intent(Intent.ACTION_VIEW,myUri);
+    	intent.addCategory(Intent.CATEGORY_DEFAULT);
+    	startActivity(intent);
+    }
+}
